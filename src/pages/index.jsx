1f1a23eb2c0ec593c429fa5d7c0bb2b4c7887a8a
Lucas Yunkyu Lee <me@luc.li>
2021-05-18 23:59:17 +0900

    feat: change index layout

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -9,39 +9,31 @@
 // ES6 import syntax
 import React from "react"
 import { Helmet } from "react-helmet"
-import styled, { css } from "styled-components"
+import styled from "styled-components"
 
 import Bubbles from "../components/bubbles"
-import { ExternalLink } from "../components/links"
 
 // Uses React functional components
 const IndexPage = () => (
   <Wrapper>
     {/* <head> management by react-helmet */}
     <Helmet lang="en" title="poapper-homepage" />
-    <Row>
-      <Col>
-        <Information>
-          <Title>Hello.</Title>
-          <p>
-            Look, a cool bubble component. Lorem ipsum dolor sit amet
-            consectetur adipisicing elit. Deserunt reiciendis eaque numquam
-            cupiditate, incidunt labore delectus aperiam nam repellat
-            consectetur perferendis saepe ratione debitis atque voluptatibus
-            totam aliquam necessitatibus. Et.
-          </p>
-        </Information>
-      </Col>
-      <Col bubble>
-        <Bubbles width="100%" height="auto" />
-      </Col>
-    </Row>
+    <Information>
+      <Title>Hello.</Title>
+      <p>
+        Look, a cool bubble component. Lorem ipsum dolor sit amet consectetur
+        adipisicing elit. Deserunt reiciendis eaque numquam cupiditate, incidunt
+        labore delectus aperiam nam repellat consectetur perferendis saepe
+        ratione debitis atque voluptatibus totam aliquam necessitatibus. Et.
+      </p>
+    </Information>
+    <StyledBubbles width="100%" height="auto" />
   </Wrapper>
 )
 
 const Wrapper = styled.div`
   display: flex;
-  flex-direction: column;
+  flex-direction: row;
   justify-content: center;
   box-sizing: border-box;
   width: 100%;
@@ -51,37 +43,14 @@ const Wrapper = styled.div`
   padding: 0 4rem;
 
   @media only screen and (max-width: 70rem) {
-    padding: 1rem 1rem;
+    flex-direction: column-reverse;
+    justify-content: space-between;
+    padding: 1rem;
   }
-`
 
-const Row = styled.div`
-  display: flex;
-  flex-direction: row;
-  @media only screen and (max-width: 70rem) {
-    flex-wrap: wrap-reverse;
+  @media only screen and (max-width: 40rem) {
+    padding: 0;
   }
-`
-
-const Col = styled.div`
-  display: flex;
-  flex-basis: 100%;
-  flex-direction: column;
-  flex-grow: 1;
-  justify-content: center;
-
-  ${props =>
-    props.bubble &&
-    `
-      @media only screen and (max-width: 70rem) {
-        margin: -2rem;
-        max-width: 35rem;
-      }
-      
-      @media only screen and (max-width: 40rem) {
-        max-width: unset;
-      }
-    `}
 `
 
 // Example of CSS-in-JS by styled-components
@@ -95,15 +64,41 @@ const Title = styled.h1`
 `
 
 const Information = styled.div`
-  width: 70%;
+  display: flex;
+  flex: 0 1 auto;
+  flex-direction: column;
+  justify-content: center;
+  margin: 0 7rem 0 0;
 
   @media only screen and (max-width: 70rem) {
-    margin: 6rem 0 0 2rem;
+    flex-basis: auto;
+    max-width: 50rem;
+    margin: 0;
   }
 
   @media only screen and (max-width: 40rem) {
     width: 100%;
-    margin: 6rem 0 0 0;
+    margin: 0;
+  }
+`
+
+const StyledBubbles = styled(Bubbles)`
+  display: flex;
+  flex: 1 0 auto;
+  flex-basis: 50%;
+  flex-direction: column;
+  justify-content: center;
+
+  @media only screen and (max-width: 70rem) {
+    max-width: 35rem;
+    margin: 0 0 0 -2rem;
+  }
+
+  @media only screen and (max-width: 40rem) {
+    box-sizing: border-box;
+    width: 100vw;
+    margin: -1rem;
+    padding: 0.5rem;
   }
 `
 
