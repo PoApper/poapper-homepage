1b45d075c04a02ac63c4499ae0dc1e38cd27eb13
Lucas Yunkyu Lee <me@luc.li>
2021-05-20 20:37:08 +0900

    feat: implement basic GitHub member sourcing via REST

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,3 +1,32 @@
 /**
  * Any files interfacing with the Gatsby Node API goes here.
  */
+
+const { Octokit } = require("@octokit/core")
+
+const octokit = new Octokit()
+
+exports.sourceNodes = async ({
+  actions,
+  createNodeId,
+  createContentDigest,
+}) => {
+  const { createNode } = actions
+
+  const res = await octokit.request("GET /orgs/{org}/members", {
+    org: "PoApper",
+  })
+
+  return res.data.map(user => {
+    const node = {
+      id: createNodeId(`${user.id}`),
+      avatar_url: user.avatar_url,
+      url: user.url,
+      internal: {
+        type: "GithubMember",
+        contentDigest: createContentDigest(user),
+      },
+    }
+    createNode(node)
+  })
+}
