83efca17e07bfe6140181133bcc16bea58e93a38
4Rem <bhyu418@postech.ac.kr>
2021-06-23 11:17:09 +0900

    start main/projectseminar

---
+++ src/components/projectseminar.jsx
@@ -0,0 +1,8 @@
+import React, { Component } from "react"
+
+class Projectseminar extends Component {
+  render() {
+    return <div>hellod</div>
+  }
+}
+export default Projectseminar
---
+++ src/pages/ps.jsx
@@ -0,0 +1,7 @@
+import React from "react"
+
+import Projectseminar from "../components/projectseminar"
+
+const Ps = () => <Projectseminar></Projectseminar>
+
+export default Ps
