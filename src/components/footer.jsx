96a33c55cfd709aa0af9ff3e3a722b2612e767dd
Lucas Yunkyu Lee <me@luc.li>
2021-06-23 22:55:18 +0900

    feat: complete footer

--- src/components/footer.jsx
+++ src/components/footer.jsx
@@ -1,31 +1,70 @@
-import { Link } from "gatsby"
 import React from "react"
-import styled, { css } from "styled-components"
-import { HiMenuAlt4 } from "react-icons/hi"
+import styled from "styled-components"
+import { FaFacebook, FaGithub } from "react-icons/fa"
+
+import { ExternalLink } from "./links"
 
 const Footer = () => {
   return (
-    <Container>
-      <FooterElement>PoApper</FooterElement>
-    </Container>
+    <FooterElement>
+      <Container>
+        <div>
+          <Addr>포항시 남구 청암로 77 학생회관 211호</Addr>
+          <ExternalLink href="mailto:staff@poapper.com">
+            staff@poapper.com
+          </ExternalLink>
+        </div>
+        <IconLinks>
+          <IconLink href="https://github.com/poapper">
+            <FaGithub size="2rem" />
+          </IconLink>
+          <IconLink href="https://www.facebook.com/poapper.club/">
+            <FaFacebook size="2rem" />
+          </IconLink>
+        </IconLinks>
+      </Container>
+      <span>© 2021 PoApper</span>
+    </FooterElement>
   )
 }
 
-const Container = styled.div`
-  display: flex;
-  justify-content: center;
-  padding: 1rem;
-  border-bottom: 1px solid #eeeeee;
+const IconLink = styled.a`
+  margin: 0 0.25rem 0 0;
+  color: ${({ theme }) => theme.foreground.sub};
 `
 
 const FooterElement = styled.footer`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  padding: 3rem 1rem;
+  color: ${({ theme }) => theme.foreground.sub};
+  border-bottom: 1px solid #eeeeee;
+`
+
+const Container = styled.div`
   display: flex;
   flex-direction: row;
   align-items: flex-start;
   justify-content: space-between;
   width: 100%;
   max-width: ${({ theme }) => theme.contentWidth};
-  margin: auto;
+  margin: 1rem auto;
+
+  @media only screen and (max-width: ${({ theme }) => theme.breakpoint.s}) {
+    flex-direction: column-reverse;
+  }
+`
+
+const Addr = styled.address`
+  font-style: normal;
+`
+
+const IconLinks = styled.div`
+  @media only screen and (max-width: ${({ theme }) => theme.breakpoint.s}) {
+    margin: 0 0 1rem 0;
+  }
 `
 
 export default Footer
