a1494aa24d29a5c7dc34c8e03a34e0cb7661a510
Lucas Yunkyu Lee <me@luc.li>
2021-06-28 23:56:07 +0900

    refactor: restructure box structure

--- src/components/boxes.jsx
+++ src/components/boxes.jsx
@@ -2,65 +2,31 @@ import { Link } from "gatsby"
 import React from "react"
 import styled from "styled-components"
 
-const Boxes = props => {
-  var list = []
-  var contents = props.content
-  var i = 0
-  while (i < contents.length) {
-    console.log(contents[i].button)
-    if (contents[i].button === "1") {
-      list.push(
-        <Box>
-          <ImageandName>
-            <Logoimage></Logoimage>
-            <Nameandcategory>
-              <Name>{contents[i].name}</Name>
-              <Category>{contents[i].category}</Category>
-            </Nameandcategory>
-          </ImageandName>
-          <ContentandButton>
-            <Content>{contents[i].text}</Content>
-            <Buttons>
-              <Link to={contents[i].link1}>
-                <Button>Open</Button>
-              </Link>
-            </Buttons>
-          </ContentandButton>
-        </Box>
-      )
-    } else if (contents[i].button === "2") {
-      list.push(
-        <Box>
-          <ImageandName>
-            <Logoimage></Logoimage>
-            <Nameandcategory>
-              <Name>{contents[i].name}</Name>
-              <Category>{contents[i].category}</Category>
-            </Nameandcategory>
-          </ImageandName>
-          <ContentandButton>
-            <Content>{contents[i].text}</Content>
-            <Buttons>
-              <Link to="/">
-                <Button>Open</Button>
-              </Link>
-              <Link to="/seminar">
-                <Button>Github</Button>
-              </Link>
-            </Buttons>
-          </ContentandButton>
-        </Box>
-      )
-    }
+const Box = ({ name, category, img, description, links }) => (
+  <BoxContainer>
+    <ImageandName>
+      <Logoimage></Logoimage>
+      <Nameandcategory>
+        <Name>{name}</Name>
+        <Category>{category}</Category>
+      </Nameandcategory>
+    </ImageandName>
+    <ContentandButton>
+      <Content>{description}</Content>
+      <Buttons>
+        {links.map(link => (
+          <Link to={link.to}>
+            <Button>{link.text}</Button>
+          </Link>
+        ))}
+      </Buttons>
+    </ContentandButton>
+  </BoxContainer>
+)
 
-    i = i + 1
-  }
+export default Box
 
-  return <div>{list}</div>
-}
-export default Boxes
-
-const Box = styled.div`
+const BoxContainer = styled.div`
   display: flex;
   margin-top: 20px;
   margin-left: 10px;
--- src/components/projectactivities.jsx
+++ src/components/projectactivities.jsx
@@ -1,22 +1,22 @@
 import React from "react"
 import styled from "styled-components"
 
-import Boxes from "./boxes"
+import Box from "./boxes"
 
 const project_contents = [
   {
     name: "project one",
     category: "web app",
-    text: "피닉스 선즈는 23일(이하 한국시간) 미국 피닉스 선즈 아레나에서 열린 2020-2021시즌 NBA 서부 콘퍼런스 파이널 2차전에서 LA 클리퍼스를 104-103으로 이겼다. 크리스 폴 빈자리가 느껴지지 않았다. 폴은 코로나19 프로토콜로 1차전에 이어 2차전도 빠졌다.하지만 폴 대신 주전 포인트가드로 나온 카메론 페인이 29득점 9어시스트 2스틸 2블록슛으로 맹활약했다. 득점과 어시스트 모두 양팀 선수 중 가",
-    button: "1",
-    link1: "/",
+    description:
+      "피닉스 선즈는 23일(이하 한국시간) 미국 피닉스 선즈 아레나에서 열린 2020-2021시즌 NBA 서부 콘퍼런스 파이널 2차전에서 LA 클리퍼스를 104-103으로 이겼다. 크리스 폴 빈자리가 느껴지지 않았다. 폴은 코로나19 프로토콜로 1차전에 이어 2차전도 빠졌다.하지만 폴 대신 주전 포인트가드로 나온 카메론 페인이 29득점 9어시스트 2스틸 2블록슛으로 맹활약했다. 득점과 어시스트 모두 양팀 선수 중 가",
+    links: [{ to: "/", text: "Open" }],
   },
   {
     name: "project two",
     category: "web",
-    text: "피닉스 선즈는 23일(이하 한국시간) 미국 피닉스 선즈 아레나에서 열린 2020-2021시즌 NBA 서부 콘퍼런스 파이널 2차전에서 LA 클리퍼스를 104-103으로 이겼다. 크리스 폴 빈자리가 느껴지지 않았다. 폴은 코로나19 프로토콜로 1차전에 이어 2차전도 빠졌다.하지만 폴 대신 주전 포인트가드로 나온 카메론 페인이 29득점 9어시스트 2스틸 2블록슛으로 맹활약했다. 득점과 어시스트 모두 양팀 선수 중 가",
-    button: "1",
-    link1: "/",
+    description:
+      "피닉스 선즈는 23일(이하 한국시간) 미국 피닉스 선즈 아레나에서 열린 2020-2021시즌 NBA 서부 콘퍼런스 파이널 2차전에서 LA 클리퍼스를 104-103으로 이겼다. 크리스 폴 빈자리가 느껴지지 않았다. 폴은 코로나19 프로토콜로 1차전에 이어 2차전도 빠졌다.하지만 폴 대신 주전 포인트가드로 나온 카메론 페인이 29득점 9어시스트 2스틸 2블록슛으로 맹활약했다. 득점과 어시스트 모두 양팀 선수 중 가",
+    links: [{ to: "/", text: "Open" }],
   },
 ]
 
@@ -24,25 +24,29 @@ const activity_contents = [
   {
     name: "activity one",
     category: "web app",
-    text: "피닉스 선즈는 23일(이하 한국시간) 미국 피닉스 선즈 아레나에서 열린 2020-2021시즌 NBA 서부 콘퍼런스 파이널 2차전에서 LA 클리퍼스를 104-103으로 이겼다. 크리스 폴 빈자리가 느껴지지 않았다. 폴은 코로나19 프로토콜로 1차전에 이어 2차전도 빠졌다.하지만 폴 대신 주전 포인트가드로 나온 카메론 페인이 29득점 9어시스트 2스틸 2블록슛으로 맹활약했다. 득점과 어시스트 모두 양팀 선수 중 가",
-    button: "2",
-    link1: "/",
-    link2: "/seminar",
+    description:
+      "피닉스 선즈는 23일(이하 한국시간) 미국 피닉스 선즈 아레나에서 열린 2020-2021시즌 NBA 서부 콘퍼런스 파이널 2차전에서 LA 클리퍼스를 104-103으로 이겼다. 크리스 폴 빈자리가 느껴지지 않았다. 폴은 코로나19 프로토콜로 1차전에 이어 2차전도 빠졌다.하지만 폴 대신 주전 포인트가드로 나온 카메론 페인이 29득점 9어시스트 2스틸 2블록슛으로 맹활약했다. 득점과 어시스트 모두 양팀 선수 중 가",
+    links: [
+      { to: "/seminar", text: "Open" },
+      { to: "https://github.com/poapper", text: "GitHub" },
+    ],
   },
   {
     name: "activity two",
     category: "web",
-    text: "피닉스 선즈는 23일(이하 한국시간) 미국 피닉스 선즈 아레나에서 열린 2020-2021시즌 NBA 서부 콘퍼런스 파이널 2차전에서 LA 클리퍼스를 104-103으로 이겼다. 크리스 폴 빈자리가 느껴지지 않았다. 폴은 코로나19 프로토콜로 1차전에 이어 2차전도 빠졌다.하지만 폴 대신 주전 포인트가드로 나온 카메론 페인이 29득점 9어시스트 2스틸 2블록슛으로 맹활약했다. 득점과 어시스트 모두 양팀 선수 중 가",
-    button: "2",
-    link1: "/",
-    link2: "/seminar",
+    description:
+      "피닉스 선즈는 23일(이하 한국시간) 미국 피닉스 선즈 아레나에서 열린 2020-2021시즌 NBA 서부 콘퍼런스 파이널 2차전에서 LA 클리퍼스를 104-103으로 이겼다. 크리스 폴 빈자리가 느껴지지 않았다. 폴은 코로나19 프로토콜로 1차전에 이어 2차전도 빠졌다.하지만 폴 대신 주전 포인트가드로 나온 카메론 페인이 29득점 9어시스트 2스틸 2블록슛으로 맹활약했다. 득점과 어시스트 모두 양팀 선수 중 가",
+    links: [
+      { to: "/seminar", text: "Open" },
+      { to: "https://github.com/poapper", text: "GitHub" },
+    ],
   },
   {
     name: "activity three",
     category: "web",
-    text: "피닉스 선즈는 23일(이하 한국시간) 미국 피닉스 선즈 아레나에서 열린 2020-2021시즌 NBA 서부 콘퍼런스 파이널 2차전에서 LA 클리퍼스를 104-103으로 이겼다. 크리스 폴 빈자리가 느껴지지 않았다. 폴은 코로나19 프로토콜로 1차전에 이어 2차전도 빠졌다.하지만 폴 대신 주전 포인트가드로 나온 카메론 페인이 29득점 9어시스트 2스틸 2블록슛으로 맹활약했다. 득점과 어시스트 모두 양팀 선수 중 가",
-    button: "1",
-    link1: "/",
+    description:
+      "피닉스 선즈는 23일(이하 한국시간) 미국 피닉스 선즈 아레나에서 열린 2020-2021시즌 NBA 서부 콘퍼런스 파이널 2차전에서 LA 클리퍼스를 104-103으로 이겼다. 크리스 폴 빈자리가 느껴지지 않았다. 폴은 코로나19 프로토콜로 1차전에 이어 2차전도 빠졌다.하지만 폴 대신 주전 포인트가드로 나온 카메론 페인이 29득점 9어시스트 2스틸 2블록슛으로 맹활약했다. 득점과 어시스트 모두 양팀 선수 중 가",
+    links: [{ to: "/", text: "Open" }],
   },
 ]
 
@@ -51,12 +55,26 @@ const Projectactivities = () => {
     <div>
       <Project>
         <Head>Project</Head>
-        <Boxes content={project_contents}></Boxes>
+        {project_contents.map(content => (
+          <Box
+            name={content.name}
+            category={content.category}
+            description={content.description}
+            links={content.links}
+          />
+        ))}
       </Project>
 
       <Project>
         <Head>Activity</Head>
-        <Boxes content={activity_contents}></Boxes>
+        {activity_contents.map(content => (
+          <Box
+            name={content.name}
+            category={content.category}
+            description={content.description}
+            links={content.links}
+          />
+        ))}
       </Project>
     </div>
   )
