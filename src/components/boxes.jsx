9ecedb89efc8ec89a6ffb539283f879b39e82264
4Rem <bhyu418@postech.ac.kr>
2021-06-28 17:40:25 +0900

    make class components to  functional components

--- src/components/boxes.jsx
+++ src/components/boxes.jsx
@@ -1,64 +1,62 @@
 import { Link } from "gatsby"
-import React, { Component } from "react"
+import React from "react"
 import styled, { css } from "styled-components"
 
-class Boxes extends Component {
-  render() {
-    var list = []
-    var contents = this.props.content
-    var i = 0
-    while (i < contents.length) {
-      console.log(contents[i].button)
-      if (contents[i].button === "1") {
-        list.push(
-          <Box>
-            <ImageandName>
-              <Logoimage></Logoimage>
-              <Nameandcategory>
-                <Name>{contents[i].name}</Name>
-                <Category>{contents[i].category}</Category>
-              </Nameandcategory>
-            </ImageandName>
-            <ContentandButton>
-              <Content>{contents[i].text}</Content>
-              <Buttons>
-                <Link to={contents[i].link1}>
-                  <Button>Open</Button>
-                </Link>
-              </Buttons>
-            </ContentandButton>
-          </Box>
-        )
-      } else if (contents[i].button === "2") {
-        list.push(
-          <Box>
-            <ImageandName>
-              <Logoimage></Logoimage>
-              <Nameandcategory>
-                <Name>{contents[i].name}</Name>
-                <Category>{contents[i].category}</Category>
-              </Nameandcategory>
-            </ImageandName>
-            <ContentandButton>
-              <Content>{contents[i].text}</Content>
-              <Buttons>
-                <Link to="/">
-                  <Button>Open</Button>
-                </Link>
-                <Link to="/seminar">
-                  <Button>Github</Button>
-                </Link>
-              </Buttons>
-            </ContentandButton>
-          </Box>
-        )
-      }
-
-      i = i + 1
+const Boxes = props => {
+  var list = []
+  var contents = props.content
+  var i = 0
+  while (i < contents.length) {
+    console.log(contents[i].button)
+    if (contents[i].button === "1") {
+      list.push(
+        <Box>
+          <ImageandName>
+            <Logoimage></Logoimage>
+            <Nameandcategory>
+              <Name>{contents[i].name}</Name>
+              <Category>{contents[i].category}</Category>
+            </Nameandcategory>
+          </ImageandName>
+          <ContentandButton>
+            <Content>{contents[i].text}</Content>
+            <Buttons>
+              <Link to={contents[i].link1}>
+                <Button>Open</Button>
+              </Link>
+            </Buttons>
+          </ContentandButton>
+        </Box>
+      )
+    } else if (contents[i].button === "2") {
+      list.push(
+        <Box>
+          <ImageandName>
+            <Logoimage></Logoimage>
+            <Nameandcategory>
+              <Name>{contents[i].name}</Name>
+              <Category>{contents[i].category}</Category>
+            </Nameandcategory>
+          </ImageandName>
+          <ContentandButton>
+            <Content>{contents[i].text}</Content>
+            <Buttons>
+              <Link to="/">
+                <Button>Open</Button>
+              </Link>
+              <Link to="/seminar">
+                <Button>Github</Button>
+              </Link>
+            </Buttons>
+          </ContentandButton>
+        </Box>
+      )
     }
 
-    return <div>{list}</div>
+    i = i + 1
   }
+
+  return <div>{list}</div>
 }
 export default Boxes
 
--- src/components/projectactivities.jsx
+++ src/components/projectactivities.jsx
@@ -1,72 +1,66 @@
 import { Link } from "gatsby"
-import React, { Component } from "react"
+import React, { useState } from "react"
 import styled, { css } from "styled-components"
 
 import Boxes from "./boxes"
 
-class Projectactivities extends Component {
-  constructor(props) {
-    super(props)
-    this.state = {
-      project_contents: [
-        {
-          name: "project one",
-          category: "web app",
-          text: "피닉스 선즈는 23일(이하 한국시간) 미국 피닉스 선즈 아레나에서 열린 2020-2021시즌 NBA 서부 콘퍼런스 파이널 2차전에서 LA 클리퍼스를 104-103으로 이겼다. 크리스 폴 빈자리가 느껴지지 않았다. 폴은 코로나19 프로토콜로 1차전에 이어 2차전도 빠졌다.하지만 폴 대신 주전 포인트가드로 나온 카메론 페인이 29득점 9어시스트 2스틸 2블록슛으로 맹활약했다. 득점과 어시스트 모두 양팀 선수 중 가",
-          button: "1",
-          link1: "/",
-        },
-        {
-          name: "project two",
-          category: "web",
-          text: "피닉스 선즈는 23일(이하 한국시간) 미국 피닉스 선즈 아레나에서 열린 2020-2021시즌 NBA 서부 콘퍼런스 파이널 2차전에서 LA 클리퍼스를 104-103으로 이겼다. 크리스 폴 빈자리가 느껴지지 않았다. 폴은 코로나19 프로토콜로 1차전에 이어 2차전도 빠졌다.하지만 폴 대신 주전 포인트가드로 나온 카메론 페인이 29득점 9어시스트 2스틸 2블록슛으로 맹활약했다. 득점과 어시스트 모두 양팀 선수 중 가",
-          button: "1",
-          link1: "/",
-        },
-      ],
+const Projectactivities = () => {
+  const [project_contents, setProject] = useState([
+    {
+      name: "project one",
+      category: "web app",
+      text: "피닉스 선즈는 23일(이하 한국시간) 미국 피닉스 선즈 아레나에서 열린 2020-2021시즌 NBA 서부 콘퍼런스 파이널 2차전에서 LA 클리퍼스를 104-103으로 이겼다. 크리스 폴 빈자리가 느껴지지 않았다. 폴은 코로나19 프로토콜로 1차전에 이어 2차전도 빠졌다.하지만 폴 대신 주전 포인트가드로 나온 카메론 페인이 29득점 9어시스트 2스틸 2블록슛으로 맹활약했다. 득점과 어시스트 모두 양팀 선수 중 가",
+      button: "1",
+      link1: "/",
+    },
+    {
+      name: "project two",
+      category: "web",
+      text: "피닉스 선즈는 23일(이하 한국시간) 미국 피닉스 선즈 아레나에서 열린 2020-2021시즌 NBA 서부 콘퍼런스 파이널 2차전에서 LA 클리퍼스를 104-103으로 이겼다. 크리스 폴 빈자리가 느껴지지 않았다. 폴은 코로나19 프로토콜로 1차전에 이어 2차전도 빠졌다.하지만 폴 대신 주전 포인트가드로 나온 카메론 페인이 29득점 9어시스트 2스틸 2블록슛으로 맹활약했다. 득점과 어시스트 모두 양팀 선수 중 가",
+      button: "1",
+      link1: "/",
+    },
+  ])
 
-      activity_contents: [
-        {
-          name: "activity one",
-          category: "web app",
-          text: "피닉스 선즈는 23일(이하 한국시간) 미국 피닉스 선즈 아레나에서 열린 2020-2021시즌 NBA 서부 콘퍼런스 파이널 2차전에서 LA 클리퍼스를 104-103으로 이겼다. 크리스 폴 빈자리가 느껴지지 않았다. 폴은 코로나19 프로토콜로 1차전에 이어 2차전도 빠졌다.하지만 폴 대신 주전 포인트가드로 나온 카메론 페인이 29득점 9어시스트 2스틸 2블록슛으로 맹활약했다. 득점과 어시스트 모두 양팀 선수 중 가",
-          button: "2",
-          link1: "/",
-          link2: "/seminar",
-        },
-        {
-          name: "activity two",
-          category: "web",
-          text: "피닉스 선즈는 23일(이하 한국시간) 미국 피닉스 선즈 아레나에서 열린 2020-2021시즌 NBA 서부 콘퍼런스 파이널 2차전에서 LA 클리퍼스를 104-103으로 이겼다. 크리스 폴 빈자리가 느껴지지 않았다. 폴은 코로나19 프로토콜로 1차전에 이어 2차전도 빠졌다.하지만 폴 대신 주전 포인트가드로 나온 카메론 페인이 29득점 9어시스트 2스틸 2블록슛으로 맹활약했다. 득점과 어시스트 모두 양팀 선수 중 가",
-          button: "2",
-          link1: "/",
-          link2: "/seminar",
-        },
-        {
-          name: "activity three",
-          category: "web",
-          text: "피닉스 선즈는 23일(이하 한국시간) 미국 피닉스 선즈 아레나에서 열린 2020-2021시즌 NBA 서부 콘퍼런스 파이널 2차전에서 LA 클리퍼스를 104-103으로 이겼다. 크리스 폴 빈자리가 느껴지지 않았다. 폴은 코로나19 프로토콜로 1차전에 이어 2차전도 빠졌다.하지만 폴 대신 주전 포인트가드로 나온 카메론 페인이 29득점 9어시스트 2스틸 2블록슛으로 맹활약했다. 득점과 어시스트 모두 양팀 선수 중 가",
-          button: "1",
-          link1: "/",
-        },
-      ],
-    }
-  }
-  render() {
-    return (
-      <div>
-        <Project>
-          <Head>Project</Head>
-          <Boxes content={this.state.project_contents}></Boxes>
-        </Project>
+  const [activity_contents, setActivitiy] = useState([
+    {
+      name: "activity one",
+      category: "web app",
+      text: "피닉스 선즈는 23일(이하 한국시간) 미국 피닉스 선즈 아레나에서 열린 2020-2021시즌 NBA 서부 콘퍼런스 파이널 2차전에서 LA 클리퍼스를 104-103으로 이겼다. 크리스 폴 빈자리가 느껴지지 않았다. 폴은 코로나19 프로토콜로 1차전에 이어 2차전도 빠졌다.하지만 폴 대신 주전 포인트가드로 나온 카메론 페인이 29득점 9어시스트 2스틸 2블록슛으로 맹활약했다. 득점과 어시스트 모두 양팀 선수 중 가",
+      button: "2",
+      link1: "/",
+      link2: "/seminar",
+    },
+    {
+      name: "activity two",
+      category: "web",
+      text: "피닉스 선즈는 23일(이하 한국시간) 미국 피닉스 선즈 아레나에서 열린 2020-2021시즌 NBA 서부 콘퍼런스 파이널 2차전에서 LA 클리퍼스를 104-103으로 이겼다. 크리스 폴 빈자리가 느껴지지 않았다. 폴은 코로나19 프로토콜로 1차전에 이어 2차전도 빠졌다.하지만 폴 대신 주전 포인트가드로 나온 카메론 페인이 29득점 9어시스트 2스틸 2블록슛으로 맹활약했다. 득점과 어시스트 모두 양팀 선수 중 가",
+      button: "2",
+      link1: "/",
+      link2: "/seminar",
+    },
+    {
+      name: "activity three",
+      category: "web",
+      text: "피닉스 선즈는 23일(이하 한국시간) 미국 피닉스 선즈 아레나에서 열린 2020-2021시즌 NBA 서부 콘퍼런스 파이널 2차전에서 LA 클리퍼스를 104-103으로 이겼다. 크리스 폴 빈자리가 느껴지지 않았다. 폴은 코로나19 프로토콜로 1차전에 이어 2차전도 빠졌다.하지만 폴 대신 주전 포인트가드로 나온 카메론 페인이 29득점 9어시스트 2스틸 2블록슛으로 맹활약했다. 득점과 어시스트 모두 양팀 선수 중 가",
+      button: "1",
+      link1: "/",
+    },
+  ])
 
-        <Project>
-          <Head>Activity</Head>
-          <Boxes content={this.state.activity_contents}></Boxes>
-        </Project>
-      </div>
-    )
-  }
+  return (
+    <div>
+      <Project>
+        <Head>Project</Head>
+        <Boxes content={project_contents}></Boxes>
+      </Project>
+
+      <Project>
+        <Head>Activity</Head>
+        <Boxes content={activity_contents}></Boxes>
+      </Project>
+    </div>
+  )
 }
 export default Projectactivities
 
@@ -87,71 +81,4 @@ const Activity = styled.div`
   display: grid;
   max-width: 960px;
   margin: 0 auto;
-`
-const Box = styled.div`
-  display: grid;
-  grid-template-columns: 200px auto;
-  margin-top: 20px;
-  margin-left: 10px;
-
-  @media only screen and (max-width: ${({ theme }) => theme.breakpoint.s}) {
-    grid-template-rows: 70px;
-    grid-template-columns: auto;
-  }
-`
-const Logoimage = styled.div`
-  display: flex;
-  width: 50px;
-  height: 50px;
-  background: blue;
-  border-radius: 30%;
-`
-const ImageandName = styled.div`
-  display: grid;
-  grid-template-columns: 70px 130px;
-`
-const Nameandcategory = styled.div`
-  display: grid;
-  grid-template-rows: 23px;
-  font-size: 16px;
-`
-const Name = styled.div`
-  display: flex;
-  height: 10px;
-  color: #666666;
-  font-weight: 800;
-`
-const Category = styled.div`
-  display: flex;
-  height: 10px;
-  color: #666666;
-`
-const ContentandButton = styled.div`
-  display: grid;
-  grid-row-gap: 0px;
-  grid-template-rows: auto auto;
-`
-const Content = styled.p`
-  display: flex;
-  max-width: 700px;
-
-  @media only screen and (max-width: ${({ theme }) => theme.breakpoint.s}) {
-    font-size: 12px;
-  }
-`
-const Buttons = styled.div`
-  display: grid;
-  grid-column-gap: 7px;
-  grid-template-columns: 70px 70px;
-  height: 30px;
-`
-const Button = styled.button`
-  width: 70px;
-  height: 30px;
-  color: white;
-  font-weight: 600;
-  background: black;
-
-  border: none;
-  border-radius: 20px;
 `
