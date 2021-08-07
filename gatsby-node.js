c6b3d91685bd3f36da977c188942032dcb9eb8db
Lucas Yunkyu Lee <me@luc.li>
2021-08-08 04:37:46 +0900

    feat: source github rest data into graphql

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,3 +1,36 @@
 /**
  * Any files interfacing with the Gatsby Node API goes here.
  */
+
+require("dotenv").config()
+const { Octokit } = require("@octokit/core")
+
+exports.sourceNodes = async ({
+  actions: { createNode },
+  createContentDigest,
+}) => {
+  const octokit = new Octokit({ auth: process.env.GITHUB_TOKEN })
+  const result = await octokit.request("GET /orgs/{org}/public_members", {
+    org: "poapper",
+  })
+
+  result.data.map(async member =>
+    createNode({
+      id: member.node_id,
+      avatarUrl: member.avatar_url,
+      url: member.url,
+      name: await (
+        await octokit.request("GET /users/{username}", {
+          username: member.login,
+        })
+      ).data.name,
+      login: member.login,
+      parent: null,
+      children: [],
+      internal: {
+        type: "GithubMember",
+        contentDigest: createContentDigest(member),
+      },
+    })
+  )
+}
