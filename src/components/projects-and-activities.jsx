55b618bf7bca78cb2afca04ccfa9f9c568364956
bluehorn07 <bluehornblues@gmail.com>
2021-08-20 01:16:18 +0900

    update popo and inpostack image url

--- src/components/projects-and-activities.jsx
+++ src/components/projects-and-activities.jsx
@@ -41,8 +41,8 @@ const project_contents = [
     category: "Web",
     description:
       "포포(POPO, POstechian's POrtal)은 포스텍 총학생회의 장비/장소를 예약할 수 있는 편의 서비스입니다. 포스텍 총학생회에서 운영을 포애퍼가 개발 및 유지/보수를 진행하고 있습니다. 학교 공용 시설을 손쉽게 예약하고, 교내 학생단체와 동아리 정보 등을 확인할 수 있습니다.",
-    links: [{ to: "http://popo.postech.ac.kr", text: "Open" }],
-    img: "http://popo.postech.ac.kr/static/media/popo.f0446fd2.svg",
+    links: [{ to: "https://popo.poapper.com", text: "Open" }],
+    img: "https://raw.githubusercontent.com/PoApper/POPO-nest-api/master/assets/popo.svg",
   },
   {
     name: "인포스택",
@@ -50,7 +50,7 @@ const project_contents = [
     description:
       "2016년 개발된 인포스택(InPoStack)은 포스텍 인근의 배달 음식점에 대한 정보를 제공하는 서비스 입니다. 학생들은 쉽고 간편하게 음식점에 주문할 수 있고, 점주에게는 월별 주문 리포트를 제공하고 있습니다. 또한, PODAC 서비스와 연계하여 인포스택에 등록된 음식점 위치와 메뉴 정보를 공개 API로 제공하고 있습니다.",
     links: [{ to: "https://inpostack.poapper.com", text: "Open" }],
-    img: "http://popo.postech.ac.kr/static/media/Light.23918dcf.svg",
+    img: "https://raw.githubusercontent.com/PoApper/POPO-nest-api/master/assets/popo.svg",
   },
 ]
 
