e26fbd31d77afa5dad47b7e48511b8dcfbe51ea0
Yeonseo0419 <81864055+Yeonseo0419@users.noreply.github.com>
2021-08-24 03:30:45 +0900

    update dropdown menu. edit dummy seminar list

--- src/components/seminar-timeline.jsx
+++ src/components/seminar-timeline.jsx
@@ -2,7 +2,7 @@ import React from "react"
 import styled from "styled-components"
 import { FiDownload } from "react-icons/fi"
 import { AiOutlineSearch } from "react-icons/ai"
-import { GoChevronDown } from "react-icons/go"
+import Select from 'react-select'
 
 import { meetingSeminarList } from "./seminar-list"
 
@@ -15,7 +15,7 @@ const MeetingSeminarCard = props => (
 
     <Info>
       <Date>{props.seminar.title}</Date>|
-      <Presenter>{props.seminar.cardSubtitle}</Presenter>
+      <Presenter>{props.seminar.author} <Link href={props.seminar.author_github}>(@{props.seminar.github_id})</Link></Presenter>
     </Info>
 
     <Content>{props.seminar.cardDetailedText}</Content>
@@ -26,18 +26,14 @@ const SeminarTimeline = () => (
   <MeetingSeminar>
     <Tool>
       <Year>
-        2021 &nbsp;
-        <GoChevronDown />
+        <DropdownYear/>
       </Year>
       <Search>
         <SearchInput />
         &nbsp; | &nbsp;
         <AiOutlineSearch />
       </Search>
-      <Sort>
-        날짜 오름차순 &nbsp;
-        <GoChevronDown />
-      </Sort>
+        <DropdownSort />
     </Tool>
     <MeetingSeminarCard seminar={meetingSeminarList[0]} />
     <hr className="hrcss" />
@@ -49,23 +45,48 @@ const SeminarTimeline = () => (
   </MeetingSeminar>
 )
 
+const yearoptions = [
+  { value: 'every history', label: '전체 년도' },
+  { value: '2021', label: '2021' },
+  { value: '2020', label: '2020' }
+]
+
+const DropdownYear = () => (
+  <div style={{width:'120px'}}>
+    <Select 
+      options={yearoptions}
+      isSearchable={false}
+      defaultValue={yearoptions[0]}
+    />
+  </div>
+)
+
+const sortoptions = [
+  { value: 'date ascending', label: '날짜 오름차순' },
+  { value: 'date descending', label: '날짜 내림차순' },
+  { value: 'alphabetical', label: 'ABC순' }
+]
+
+const DropdownSort = () => (
+  <div style={{width:'160px'}}>
+  <Select 
+    options={sortoptions}
+    isSearchable={false}
+    defaultValue={sortoptions[0]}/>
+  </div>
+)
+
 const Tool = styled.div`
   display: flex;
   flex-direction: row;
+  flex-wrap: wrap;
   align-items: center;
   justify-content: flex-start;
-  font-size: 0.9rem;
+  font-size: 0.9rem;    
+  }
 `
-
 const Year = styled.div`
-  display: flex;
-  flex-direction: row;
-  align-items: center;
-  justify-content: space-around;
-  margin-right: 1rem;
-  padding: 0.3rem 0.6rem;
-  border: 1.5px solid lightgrey;
-  border-radius: 5px;
+ margin: 0 1rem 0 0;
 `
 
 const Search = styled.div`
@@ -74,8 +95,8 @@ const Search = styled.div`
   align-items: center;
   justify-content: flex-end;
   width: 14rem;
-  margin-right: 1rem;
-  padding: 0.3rem 0.6rem;
+  margin: 5px 1rem 5px 0;
+  padding: 0.25rem 0.6rem;
   border: 1.5px solid lightgrey;
   border-radius: 5px;
 `
@@ -83,16 +104,7 @@ const Search = styled.div`
 const SearchInput = styled.input`
   flex: 1;
   border: none;
-`
-
-const Sort = styled.div`
-  display: flex;
-  flex-direction: row;
-  align-items: center;
-  justify-content: flex-end;
-  padding: 0.25rem 0.6rem;
-  border: 1.5px solid lightgrey;
-  border-radius: 5px;
+  height: 20.667px
 `
 
 const MeetingSeminar = styled.div`
@@ -128,6 +140,13 @@ const Date = styled.div`
 
 const Presenter = styled.div`
   margin-left: 0.3rem;
+  color: black;
+  text-decoration: none;
+`
+
+const Link = styled.a`
+  color: black;
+  text-decoration: none;
 `
 
 const Content = styled.div`
